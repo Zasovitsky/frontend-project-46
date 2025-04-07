b46b36d645beb6c1ba14368e18c9cccf254f3c81
Zasovitsky Leo <zasovitskyleo@Noutbuk-Zasovitsky.local>
2025-04-07 15:59:54 +0300

    change tab

--- src/parser.js
+++ src/parser.js
@@ -1,9 +1,9 @@
 import yaml from 'js-yaml';
 
 const parser = {
-    json: JSON.parse,
-    yaml: yaml.load,
-    yml: yaml.load,
+  json: JSON.parse,
+  yaml: yaml.load,
+  yml: yaml.load,
 };
 
 export default (data, format) => parser[format](data);
